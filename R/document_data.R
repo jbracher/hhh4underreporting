64cc4feeac8777f7fd297540545f1f4fdb47c10b
Johannes <jbracher@outlook.de>
2019-01-29 16:25:26 +0100

    Added data.

---
+++ R/document_data.R
@@ -0,0 +1,10 @@
+#' Data set on rotavirus gastroenteritis in Bremen and Lower Saxony
+#'
+#' Case counts of rotavirus gastroenteritis in the sixteen German states, 2001-2017
+#'
+#' @name rota_germany
+#' @docType data
+#' @author Johannes Bracher
+#' @source Surveillance counts retrieved from SurvStat@RKI 2.0 service (https://survstat.rki.de), Robert Koch Institute, Berlin as of 17 Oct 2018.
+#' @keywords data
+NULL
