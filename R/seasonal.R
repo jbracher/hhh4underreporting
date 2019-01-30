236a185224d65ae562e3f6af4522da69cd18b192
Johannes <jbracher@outlook.de>
2019-01-30 18:03:56 +0100

    Added some comments to seasonal stuff.

--- R/seasonal.R
+++ R/seasonal.R
@@ -1,7 +1,8 @@
 #' @useDynLib hhh4underreporting
 #' @importFrom Rcpp sourceCpp
 
-# get stationary mean in periodic case:
+# get stationary mean in periodic case; there is also an Rcpp version,
+# this is only to check results.
 stat_mean_seas <- function(nu, phi, kappa){
   if(length(nu) != length(phi) | length(nu) != length(kappa)){
     stop("The provided nu, phi and kappa need to be of the same length.")
@@ -9,7 +10,7 @@ stat_mean_seas <- function(nu, phi, kappa){
 
   # length of period:
   L <- length(nu)
-  # set up matrix of contributions of different g-values to different means:
+  # set up matrix of contributions of different nu to different means:
   contributions <- matrix(NA, ncol = L, nrow = L)
   # fill this matrix row-wise:
   for(i in 1:L){
@@ -35,7 +36,8 @@ stat_mean_seas <- function(nu, phi, kappa){
   rowSums(contributions)
 }
 
-# get stationary variance in periodic case:
+# get stationary variance in periodic case; there is also an Rcpp version,
+# this is only to check results.
 stat_var_seas <- function(nu, phi, kappa, psi){
 
   if(length(nu) != length(phi) | length(nu) != length(kappa) | length(nu) != length(psi)){
@@ -43,14 +45,14 @@ stat_var_seas <- function(nu, phi, kappa, psi){
   }
 
   L <- length(nu)
-  a <- psi; b <- 1; c <- 0
-  a_shifted <- a[c(L, 1:(L - 1))]
+  psi_shifted <- psi[c(L, 1:(L - 1))]
   mu <- stat_mean_seas(nu, phi, kappa)
-  v <- a*mu^2 + b*mu + c
+  v <- psi*mu^2 + mu
   v_shifted <- v[c(L, 1:(L - 1))]
-  h <- (phi + kappa)^2 + phi^2*a_shifted
+  h <- (phi + kappa)^2 + phi^2*psi_shifted
   summands <- phi^2*v_shifted
 
+  # get all summands entering tinto the different var(lambda_t):
   contributions <- matrix(NA, ncol = L, nrow = L)
   for(i in 1:L){
     order_back_in_time <- if(i == L){
@@ -60,23 +62,29 @@ stat_var_seas <- function(nu, phi, kappa, psi){
     weights <- cumprod(c(1, h[order_back_in_time]))
     contributions[i, ] <- weights[1:L]*summands[order_back_in_time]/(1 - prod(weights))
   }
+  # obtain var(lambda_t) as rowSums:
   stat_var_lambda <- rowSums(contributions)
-  stat_var_X <- (1 + a)*stat_var_lambda + v
+  # move to var(X_t:)
+  stat_var_X <- (1 + psi)*stat_var_lambda + v
   return(list(stat_var_lambda = stat_var_lambda,
-              stat_var_X = stat_var_X,
-              h = h))
+              stat_var_X = stat_var_X))
 }
 
 # wrapper: get stationary properties:
 compute_sop_seas <- function(nu, phi, kappa, psi, q){
+  # mean:
   mu_X <- stat_mean_seas_cpp(nu, phi, kappa)
+  # variance:
   sigma2_X <- stat_var_seas_cpp(nu, phi, kappa, psi)$stat_var_X
+  # autocovariance for d = 1 (corresponds to g in manuscript)
   inds_shifted <- c(length(phi), 1:(length(phi) - 1))
   cov1_X <- phi*sigma2_X[inds_shifted] +
     kappa*(sigma2_X[inds_shifted] - mu_X[inds_shifted] - psi[inds_shifted]*mu_X[inds_shifted]^2)/
     (1 + psi[inds_shifted])
+  # decay of the autocovariance function (h in the manuscript):
   decay_X <- phi + kappa
 
+  # and for the thinned version:
   mu_X_tilde <- q*mu_X
   sigma2_X_tilde <- q^2*sigma2_X + q*(1 - q)*mu_X
   cov1_X_tilde <- q^2*cov1_X
@@ -92,11 +100,13 @@ compute_sop_seas <- function(nu, phi, kappa, psi, q){
 reparam_seas <- function(nu, phi, kappa, psi, q){
   L <- length(nu)
 
+  # if q == 1 nothing needs to be done:
   if(q == 1) return(list(nu = nu, phi = phi, kappa = kappa, psi = psi, q = 1))
 
+  # the second-order properties we want to achieve:
   target_sop <- compute_sop_seas(nu, phi, kappa, psi, q)
 
-  # now find a completely observed process Y with the same properties:
+  # now find a completely observed process Y with these properties:
   nu_Y <- q*nu # known for theoretical reasons
   phi_plus_kappa_Y <- target_sop$decay_cov_X_tilde # this, too.
   mu_Y <- target_sop$mu_X_tilde # by definition
@@ -114,7 +124,7 @@ reparam_seas <- function(nu, phi, kappa, psi, q){
   v_lambda_Y[L] <- (v_Y[L] - mu_Y[L] - psi_Y[L]*mu_Y[L]^2)/
     (1 + psi_Y[L])
 
-  # now we can iteratively roll through the weeks and correct the values of phi and kappa
+  # now we can iteratively roll through the weeks and correct the values of phi and psi
   for(k in 1:3){
     for(i in 1:L){
       im1 <- ifelse(i == 1, L, i - 1)
@@ -135,7 +145,7 @@ reparam_seas <- function(nu, phi, kappa, psi, q){
   return(list(nu = nu_Y, phi = phi_Y, kappa = kappa_Y, psi = psi_Y, q = 1))
 }
 
-# function for obtaining "shifted" nu necessary for geometric-lag formulation
+# function for obtaining nu_star (compare manuscript), necessary for geometric-lag formulation
 nu_to_nu_star_seas <- function(nu, kappa, max_lag = 5){
   nu_prolonged <- c(tail(nu, max_lag), nu)
   nu_matr <- matrix(nrow = length(nu), ncol = max_lag + 1)
@@ -194,11 +204,13 @@ get_weight_matrix_seas <- function(phi, kappa, max_lag){
 #' @return the return object from \code{optim} providing the maximum likelihood estimates
 #' (mostly on the log scale).
 #' @export
-fit_hhh4u_seasonal <- function(observed, L, q, seas_phi = FALSE, include_kappa = TRUE, initial = c(alpha_nu = 4, gamma_nu = 0, delta_nu = 0,
+fit_hhh4u_seasonal <- function(observed, L, q, seas_phi = FALSE, include_kappa = TRUE,
+                               initial = c(alpha_nu = 4, gamma_nu = 0, delta_nu = 0,
                                               alpha_phi = -1, gamma_phi = 0, delta_phi = 0,
                                               alpha_kappa = -1, log_psi = -3),
                          max_lag = 10, iter_optim = 3, ...){
 
+  # adapt initial values depending on which aparameters are included in the model:
   if(seas_phi == FALSE){
     initial <- initial[!(names(initial) %in% c("gamma_phi", "delta_phi"))]
   }
@@ -206,6 +218,7 @@ fit_hhh4u_seasonal <- function(observed, L, q, seas_phi = FALSE, include_kappa =
     initial <- initial[names(initial) != "alpha_kappa"]
   }
 
+  # wrapper around nllik_seas, to be passed to optim
   lik_vect <- function(pars){
     alpha_nu <- pars["alpha_nu"]
     gamma_nu <- pars["gamma_nu"]
@@ -222,7 +235,9 @@ fit_hhh4u_seasonal <- function(observed, L, q, seas_phi = FALSE, include_kappa =
              alpha_kappa = alpha_kappa, psi = psi, q = q, L = L, max_lag = max_lag)
   }
 
+  # optimize:
   opt <- optim(par = initial, fn = lik_vect,...)
+  # repeat optimizatin (improves convergence)
   for(i in 1:iter_optim){
     opt <- optim(par = opt$par, fn = lik_vect,...)
   }
@@ -247,8 +262,6 @@ fit_hhh4u_seasonal <- function(observed, L, q, seas_phi = FALSE, include_kappa =
 #'
 #' @return The negative log-likelihood as scalar or (if \code{return_contributions == TRUE}) the vector of
 #' negative log-likelihood contributions.
-#'
-#' @export
 nllik_seas <- function(observed, alpha_nu, gamma_nu, delta_nu,
                      alpha_phi, gamma_phi = 0, delta_phi = 0,
                      alpha_kappa, psi, q, L, max_lag = 5, return_contributions = FALSE){
