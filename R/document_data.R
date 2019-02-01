a0d14cf18b24a1ff5e0226c91a310094edc452be
Johannes <jbracher@outlook.de>
2019-02-01 15:58:55 +0100

    Added noro data.

--- R/document_data.R
+++ R/document_data.R
@@ -8,3 +8,14 @@
 #' @source Surveillance counts retrieved from SurvStat@RKI 2.0 service (https://survstat.rki.de), Robert Koch Institute, Berlin as of 17 Oct 2018.
 #' @keywords data
 NULL
+
+#' Data set on norovirus gastroenteritis in Germany
+#'
+#' Case counts of norovirus gastroenteritis in the sixteen German states, 2001-2017
+#'
+#' @name rota_germany
+#' @docType data
+#' @author Johannes Bracher
+#' @source Surveillance counts retrieved from SurvStat@RKI 2.0 service (https://survstat.rki.de), Robert Koch Institute, Berlin as of 17 Oct 2018.
+#' @keywords data
+NULL
